2df39d8200e75ecd570024d0c36de12709f4ba8c
mohd_ <mohd_em@hotmail.com>
2019-05-19 03:27:45 +0300

    end of episode 23 ( adding pageview model )

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,4 @@
-import 'package:flutter/material.dart';
-void main() {
-  runApp(NewsApp());
 
-}
-
-
-class NewsApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: OnBoarding(),
-    )
-  }
-}
 
 
 
