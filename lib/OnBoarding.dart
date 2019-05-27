d63312c6708a14562235d70fea76b02f9f62a1c2
mohd_ <mohd_em@hotmail.com>
2019-05-27 03:36:50 +0300

    up to minute 15 episode 25 - adding thr package of viewpage indicator , show it on the screen

--- lib/OnBoarding.dart
+++ lib/OnBoarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pagemodel.dart';
+import 'package:page_view_indicator/page_view_indicator.dart';
 
 class OnBoarding extends StatefulWidget {
   @override
@@ -6,77 +8,140 @@ class OnBoarding extends StatefulWidget {
 }
 
 class _OnBoardingState extends State<OnBoarding> {
+
+  List<PageModel> pages;
+  
+  ValueNotifier<int> _pageViewNotifier = ValueNotifier(0);
+
+
+  void _addPages(){
+    pages = List<PageModel>();
+  pages.add(PageModel('Welcome1', 'Description 1', Icons.ac_unit, 'assets/images/bg.png'));
+  pages.add(PageModel('Welcome2', 'Description 2', Icons.add_a_photo, 'assets/images/bg2.png'));
+  pages.add(PageModel('Welcome3', 'Description 3', Icons.title, 'assets/images/bg3.png'));
+  pages.add(PageModel('Welcome4', 'Description 4', Icons.add_call, 'assets/images/bg4.png'));
+  
+}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    _addPages();
+    return  Stack(
         children: <Widget>[
-          PageView.builder(
-            itemBuilder: (context, index) {
-              return Stack(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: ExactAssetImage('assets/images/bg.png'),
-                        fit: BoxFit.cover,
+          Scaffold(
+            body: PageView.builder(
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: ExactAssetImage(pages[index].image,),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Transform.translate(
-                          child: Icon(
-                            Icons.account_balance,
-                            size: 120,
-                            color: Colors.white,
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Transform.translate(
+                            child: Icon(
+                              pages[index].icon,
+                              size: 120,
+                              color: Colors.white,
+                            ),
+                            offset: Offset(0, -100),
                           ),
-                          offset: Offset(0, -100),
-                        ),
-                        Text(
-                          "welcome",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
+                          Text(
+                            pages[index].title,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          "Your Navigator to Bahrain market",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 18),
+                            child: Text(
+                              pages[index].description,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
-            itemCount: 4,
+                  ],
+                );
+              },
+              itemCount: pages.length,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0, 150),
+            child: Align(
+              alignment: Alignment.center,
+              child: _displauPageIndicators(pages.length)
+            ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
-              padding: const EdgeInsets.all(24),
-              child: RaisedButton(
-                color: Colors.red.shade800,
-                child: Text(
-                  "GET STARTED",
-                  style: TextStyle(color: Colors.white),
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: 24,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: RaisedButton(
+                  color: Colors.red.shade900,
+                  child: Text(
+                    "GET STARTED",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  onPressed: () {},
                 ),
-                onPressed: () {},
               ),
             ),
           ),
         ],
+      );
+  }
+  
+  Widget _displauPageIndicators(int length){
+    return PageViewIndicator(
+      pageIndexNotifier: _pageViewNotifier,
+      length: length,
+      normalBuilder: (animationController, index) => Circle(
+        size: 8.0,
+        color: Colors.black87,
+      ),
+      highlightedBuilder: (animationController, index) => ScaleTransition(
+        scale: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.ease,
+        ),
+        child: Circle(
+          size: 12.0,
+          color: Colors.black45,
+        ),
       ),
     );
   }
 }
+
+
+
+
