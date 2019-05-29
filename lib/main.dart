10faa0eda371dbf4b1f2e391ac776b0e19e14c78
mohd_ <mohd_em@hotmail.com>
2019-05-30 01:27:13 +0300

    end of ep25 all files

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:newa_app/OnBoarding.dart';
+import 'package:newa_app/screens/OnBoarding.dart';
+import 'screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-main(){
-runApp(NewsApp());
+main()  async {
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool seen= prefs.getBool('seen');
+
+  Widget _screen;
+
+  if(seen == null || seen == false){
+    _screen = OnBoarding();
+  }else{
+    _screen = Home_screen();
+  }
+
+runApp(NewsApp(_screen));
 }
 
 class NewsApp extends StatelessWidget {
+  final Widget _screen;
+
+  NewsApp(this._screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoarding(),
+      home: this._screen,
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home_screen extends StatefulWidget {
+  @override
+  _Home_screenState createState() => _Home_screenState();
+}
+
+class _Home_screenState extends State<Home_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('mohammed'),
+      ),
+    );
+    
+  }
+}
