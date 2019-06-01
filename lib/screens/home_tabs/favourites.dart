ba7afa37dcb42efe18970dd979d75bf9701944f1
mohd_ <mohd_em@hotmail.com>
2019-06-01 04:40:48 +0300

    end of episode 27 ( whats new screen done )

--- lib/screens/home_tabs/favourites.dart
+++ lib/screens/home_tabs/favourites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Favourites extends StatefulWidget {
   @override
@@ -6,10 +7,127 @@ class Favourites extends StatefulWidget {
 }
 
 class _FavouritesState extends State<Favourites> {
+  List<Color> colorsList = [
+    Colors.red,
+    Colors.teal,
+    Colors.deepOrange,
+    Colors.indigo,
+    Colors.brown,
+    Colors.purple,
+  ];
+
+  Random random = Random();
+
+  Color _getRandomColor() {
+    return colorsList[random.nextInt(colorsList.length)];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orangeAccent,
+    return ListView.builder(
+      itemBuilder: (context, position) {
+        return Card(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                _authorRow(),
+                SizedBox(height: 16,),
+                _newsItemRow(),
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: 20,
+    );
+  }
+
+  Widget _authorRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: ExactAssetImage('assets/images/placeholder_bg.png'),
+                    fit: BoxFit.cover),
+                shape: BoxShape.circle,
+              ),
+              width: 50,
+              height: 50,
+              margin: EdgeInsets.only(right: 16),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Michael Adams',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      '15 Min .',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      'Life Style',
+                      style: TextStyle(color: _getRandomColor()),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        IconButton(
+          icon: Icon(Icons.bookmark_border),
+          onPressed: () {},
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
+
+  Widget _newsItemRow() {
+    return Row(
+
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: ExactAssetImage('assets/images/placeholder_bg.png'),
+                fit: BoxFit.cover),
+          ),
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.only(right: 16),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Tech Tent: Old phones and safe social ',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                maxLines: 2,
+              ),
+              Text(
+                'we also talk about the future of work as the robot advance , and we ask whether a retro phone ',
+                style: TextStyle(color: Colors.grey, fontSize: 14,height: 1.25),),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
