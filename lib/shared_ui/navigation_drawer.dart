63483ce55e91da19706ca46b4bddd16135365305
mohd_ <mohd_em@hotmail.com>
2019-06-01 05:03:08 +0300

    end of episode 28 - done with popular screen and favourited screen

--- lib/shared_ui/navigation_drawer.dart
+++ lib/shared_ui/navigation_drawer.dart
@@ -6,8 +6,44 @@ class navigationDrawer extends StatefulWidget {
 }
 
 class _navigationDrawerState extends State<navigationDrawer> {
+  List<String> navMenu = [
+    'Explore',
+    'Headlines News',
+    'Read Later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 6, left: 24),
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return Padding(
+              padding: const EdgeInsets.all(4),
+              child: ListTile(
+                title: Text(
+                  navMenu[position],
+                  style: TextStyle(
+                    color: Colors.grey.shade800,
+                    fontSize: 20,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey.shade400,
+                ),
+                onTap: () {},
+              ),
+            );
+          },
+          itemCount: navMenu.length,
+        ),
+      ),
+    );
   }
 }
