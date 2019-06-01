6da2dc327994bc58d40c46a75b0ba53d2c162ea9
mohd_ <mohd_em@hotmail.com>
2019-06-01 03:14:40 +0300

    end of episode 27 ( whats new screen done )

--- lib/screens/home_tabs/popular.dart
+++ lib/screens/home_tabs/popular.dart
@@ -8,8 +8,63 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return ListView.builder(itemBuilder: (context , position){
+      return Card(
+        child: _drawSingleRow() ,
+      );
+    }, itemCount: 20,);
+  }
+
+
+  Widget _drawSingleRow() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            child: Image(
+              image: ExactAssetImage('assets/images/placeholder_bg.png'),
+              fit: BoxFit.cover,
+            ),
+            width: 100,
+            height: 100,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'The World Global Warming Annual Summit',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Michael Adams '),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.timer),
+                        Text('15 min'),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
